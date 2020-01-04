3611c6c14eb9ef8cf225df151f067c7ddf8c7f77
takanorimatuo <regional.portals.wakayama@gmail.com>
2020-01-04 17:41:45 +0900

    自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
   function buildHTML(message){
-    if (message.image.url) {
+    if (message.content && message.image ) {
       var html = `<div class="message" data-message-id=${message.id}>
                     <div class="upper-message">
                       <div class="upper-message__user-name">
@@ -16,11 +16,11 @@ $(function(){
                       </p>
                     </div>
                     <div class="lower-message__image">
-                        <img src= "${ message.image.url }">
+                      <img src= "${ message.image}">
                     </div>
                   </div>`
       return html;
-    } else {
+    } else if (message.content) {
       var html = `<div class="message" data-message-id=${message.id}>
                     <div class="upper-message">
                       <div class="upper-message__user-name">
@@ -37,12 +37,29 @@ $(function(){
                     </div>
                   </div>`
       return html;
+    } else if (message.image) {
+      var html = `<div class="message" data-message-id=${message.id}>
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${ message.user_name }
+                      </div>
+                      <div class="upper-message__date">
+                        ${ message.created_at }
+                      </div>
+                    </div>
+                    <div class="lower-message__image">
+                      <img src= "${ message.image}">
+                    </div>
+                  </div>`
+      return html;
     };
+   
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
+    last_message_id = $('.message:last').data("message-id");
     $.ajax({
       url: url,
       type: 'POST',
@@ -62,6 +79,33 @@ $(function(){
     })
     .always(function(){
       $('.form_box_submit').prop('disabled', false);
+    });
+  });
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
     })
-  })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat_box').append(insertHTML);
+        $('.chat_box').animate({ scrollTop: $('.chat_box')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".form__submit").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
